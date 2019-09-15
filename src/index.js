cbeb40ad093bce698755102dbb12f710fd24856a
Kalysh <503292@ukr.net>
2019-09-15 21:57:23 +0300

    MUST-FIX _ need make friends masonry and infinityScroll

--- src/index.js
+++ src/index.js
@@ -1,8 +1,23 @@
 import './styles.css';
+
 import InfiniteScroll from 'infinite-scroll';
 import photoTemplate from './templates/photo.hbs';
 
-// console.log(InfiniteScroll);
+import Masonry from 'masonry-layout';
+
+const masonryInstance = new Masonry('#feed', {
+  columnWidth: '.grid-sizer',
+  itemSelector: '.grid-item',
+  percentPosition: true,
+  transitionDuration: '0.3s',
+  // gutter: 10,
+  // stagger: 30,
+  visibleStyle: { transform: 'translateY(0)', opacity: 1 },
+  hiddenStyle: { transform: 'translateY(100px)', opacity: 0 },
+});
+
+
+console.log(masonryInstance);
 
 //================
 // infinite scroll
@@ -13,23 +28,19 @@ const infScrollInstance = new InfiniteScroll(feedContainer, {
     return `https://cors-anywhere.herokuapp.com/https://pixabay.com/api/?key=13631456-c9d9ce7db3701f221793ce82d&q=cat&image_type=photo&page=${this.pageIndex}`;
   },
   responseType: 'text',
-  // append: '.post',
   history: false,
+  outlayer: masonryInstance,
 });
-// console.log(infScrollInstance.pageIndex);
+console.log(infScrollInstance);
 
 infScrollInstance.on('load', (response, url) => {
   const images = JSON.parse(response);
-
-  // console.group(url);
-  // console.log(images);
-  // console.groupEnd(url);
 
   console.log(images);
 
   const markup = images.hits.map(hit => photoTemplate(hit)).join('');
 
-  console.log(markup);
+  // console.log(markup);
   const proxyEl = document.createElement('div');
   proxyEl.innerHTML = markup;
 
@@ -37,7 +48,7 @@ infScrollInstance.on('load', (response, url) => {
   // const markup = posts.map(post => postTemplate(post)).join('');
   // // через reduce #2
   // // const markup2 = posts.reduce((acc, post) => acc + postTemplate(post), '');
-  // // console.log(markup);
+  // console.log(markup);
 
 
   const parseItems = proxyEl.querySelectorAll('.item-photo');
