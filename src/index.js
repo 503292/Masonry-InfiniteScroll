8c798be83bf8587c674306fa79c45533ac06d413
Kalysh <503292@ukr.net>
2019-09-15 18:04:38 +0300

    ADD _ from pixabay random foto with cats

--- src/index.js
+++ src/index.js
@@ -1,20 +1,49 @@
 import './styles.css';
-import InfiniteScroll from 'infinite-scroll'
-console.log(InfiniteScroll);
+import InfiniteScroll from 'infinite-scroll';
+import postTemplate from './templates/post.hbs';
+
+// console.log(InfiniteScroll);
 
 //================
 // infinite scroll
 const feedContainer = document.querySelector('#feed');
-// const infScroll = new InfiniteScroll( feedContainer, {
-//   // options
-//   path: '.pagination__next',
-//   append: '.post',
-//   history: false,
-// });
+const infScrollInstance = new InfiniteScroll(feedContainer, {
+  // options
+  path() {
+    return `https://cors-anywhere.herokuapp.com/https://pixabay.com/api/?key=13631456-c9d9ce7db3701f221793ce82d&q=cat&image_type=photo&page=${this.pageIndex}`;
+  },
+  responseType: 'text',
+  // append: '.post',
+  history: false,
+});
+// console.log(infScrollInstance.pageIndex);
 
-// element argument can be a selector string
-//   for an individual element
-// const infScroll = new InfiniteScroll( '.container', {
-//   // options
-// });
+infScrollInstance.on('load', (response, url) => {
+  const images = JSON.parse(response);
 
+  // console.group(url);
+  // console.log(images);
+  // console.groupEnd(url);
+
+  console.log(images);
+
+  const markup = images.hits.map(hit => postTemplate(hit)).join('');
+
+  console.log(markup);
+  const proxyEl = document.createElement('div');
+  proxyEl.innerHTML = markup;
+
+  // // через map #1
+  // const markup = posts.map(post => postTemplate(post)).join('');
+  // // через reduce #2
+  // // const markup2 = posts.reduce((acc, post) => acc + postTemplate(post), '');
+  // // console.log(markup);
+
+
+  console.log(proxyEl);
+  console.log(proxyEl.children);
+
+  infScrollInstance.appendItems(proxyEl.children);
+});
+
+infScrollInstance.loadNextPage();
