f70d348a50ae9d34746b741f2f026c84b3840e2e
Kalysh <503292@ukr.net>
2019-09-15 15:34:09 +0300

    ADD _ infinite-scroll

--- src/index.js
+++ src/index.js
@@ -1,51 +1,20 @@
-import Masonry from 'masonry-layout';
-import imagesLoaded from 'imagesloaded';
-import makeGridItem from './makeGridItem';
-
-
-
 import './styles.css';
+import InfiniteScroll from 'infinite-scroll'
+console.log(InfiniteScroll);
 
-const masonryInstance = new Masonry('#gallery', {
-  columnWidth: '.grid-sizer',
-  itemSelector: '.grid-item',
-  percentPosition: true,
-  transitionDuration: '0.3s',
-  // stagger: 30,
-});
+//================
+// infinite scroll
+const feedContainer = document.querySelector('#feed');
+// const infScroll = new InfiniteScroll( feedContainer, {
+//   // options
+//   path: '.pagination__next',
+//   append: '.post',
+//   history: false,
+// });
 
-imagesLoaded('#gallery').on('progress', () => {
-  masonryInstance.layout();
-});
+// element argument can be a selector string
+//   for an individual element
+// const infScroll = new InfiniteScroll( '.container', {
+//   // options
+// });
 
-fetch(
-  'https://pixabay.com/api/?key=13631456-c9d9ce7db3701f221793ce82d&q=dog&image_type=photo',
-)
-  .then(res => res.json())
-  .then(({ hits }) => {
-    const elements = hits.map(hit => makeGridItem(hit.webformatURL));
-    // добавити в DOM
-    document.querySelector('#gallery').append(...elements);
-    // добавити в Masonry для відслідковування
-    masonryInstance.addItems(elements);
-
-    imagesLoaded('#gallery').on(
-      'progress',
-      masonryInstance.layout.bind(masonryInstance),
-    );
-  });
-
-//=====================================
-// add  PNptyfy
-import PNotify from 'pnotify/dist/es/PNotify';
-import PNotifyButtons from 'pnotify/dist/es/PNotifyButtons';
-import 'pnotify/dist/PNotifyBrightTheme.css';
-import PNotifyStyleMaterial from 'pnotify/dist/es/PNotifyStyleMaterial.js';
-import 'material-design-icons/iconfont/material-icons.css';
-
-// Set default styling.
-PNotify.defaults.styling = 'material';
-// This icon setting requires the Material Icons font. (See below.)
-PNotify.defaults.icons = 'material';
-
-PNotify.alert('4а 4а 4а');
---
+++ src/js/pnotyfi.js
@@ -0,0 +1,14 @@
+//=====================================
+// add  PNptyfy
+import PNotify from 'pnotify/dist/es/PNotify';
+import PNotifyButtons from 'pnotify/dist/es/PNotifyButtons';
+import 'pnotify/dist/PNotifyBrightTheme.css';
+import PNotifyStyleMaterial from 'pnotify/dist/es/PNotifyStyleMaterial.js';
+import 'material-design-icons/iconfont/material-icons.css';
+
+// // Set default styling.
+// PNotify.defaults.styling = 'material';
+// // This icon setting requires the Material Icons font. (See below.)
+// PNotify.defaults.icons = 'material';
+
+// PNotify.alert('4а 4а 4а');
